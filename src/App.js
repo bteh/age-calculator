12ad5e03a560b096f9e9db39ad8c198802da2924
Brian Teh <brianteh95@gmail.com>
2018-09-09 13:53:36 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap';
+import './App.css'
+
+
+class App extends Component {
+	constructor(){
+		super();
+		this.state = {
+			newDate: '',
+			birthday: '1994-06-20'
+		}
+	}
+
+  changeBirthday(){
+  	console.log(this.state)
+  	this.setState({birthday: this.state.newDate})
+  }
+
+
+	render(){
+		return(
+		<div className='App'> 
+			<Form inline>
+			<h2>Input your Birthday</h2>
+				<FormControl type = 'date' 
+				onChange = {(event) => {
+					this.setState({ newDate: event.target.value})
+				}}>
+				</FormControl>
+				{' '}
+				<Button onClick = {() => this.changeBirthday()}>
+				  Submit
+				</Button>
+			</Form> 
+		</div>
+			)
+	}
+}
+
+export default App;
